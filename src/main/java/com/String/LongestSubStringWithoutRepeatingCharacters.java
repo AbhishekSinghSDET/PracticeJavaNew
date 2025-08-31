ac9f4d916044116aee6f162588f640f0b70df696
abhi2560 <abhisingh2560@gmail.com>
2025-08-31 12:16:44 +0530

    Initial Commit

--- src/main/java/com/String/LongestSubStringWithoutRepeatingCharacters.java
+++ src/main/java/com/String/LongestSubStringWithoutRepeatingCharacters.java
@@ -1,4 +1,30 @@
 package com.String;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LongestSubStringWithoutRepeatingCharacters {
+
+    public static int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<>(); // store unique characters
+        int left = 0; // left pointer
+        int maxLen = 0;
+
+        for (int right = 0; right < s.length(); right++) { // right pointer expands
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left)); // shrink window from left
+                left++;
+            }
+            set.add(s.charAt(right)); // add new char
+            maxLen = Math.max(maxLen, right - left + 1); // update max
+        }
+
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("abcabcbb")); // 3 ("abc")
+        System.out.println(lengthOfLongestSubstring("bbbbb"));    // 1 ("b")
+        System.out.println(lengthOfLongestSubstring("pwwkew"));   // 3 ("wke")
+    }
 }
